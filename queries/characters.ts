fdcad664360f7995347b7ce199f1a242c79b309f
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-12-03 22:31:31 +0600

    rendering data

---
+++ queries/characters.ts
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+
+
+
+const GET_CHARACTERS = gql`
+
+query GetCharacters{
+characters{
+    results{
+        id
+        name
+        image
+    }
+}
+}
+
+`
